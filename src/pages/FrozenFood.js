2219870f64d192b1afc9dc8aa8233b0e9a511cfa
Jonathan Clegg <jonathanclegg928@gmail.com>
2019-04-01 23:35:28 +0100

    add frozen food product avatars

--- src/pages/FrozenFood.js
+++ src/pages/FrozenFood.js
@@ -1,21 +1,138 @@
 import React from 'react'
 import {data} from '../components/GetData';
+import styled from 'styled-components'
+import Image from '../components/Image.js'
+import mouse from '../assets/mouseother.jpg'
+
+const Parent = styled.div`
+    display: grid;
+    margin-top: 30px;
+    grid-template-columns: 1fr 1fr;
+`
 
 class FrozenFood extends React.Component {
-  state = {
-    products: []
+  constructor(props) {
+    super(props)
+    this.state = {
+       products: []
+    }
   }
 
   componentDidMount() {
     data().then(data=>{ 
       this.setState({
-           products: data
-       })
-   });
- }
+        products: data
+      })
+    });
+  }
   render() {
     return (
-      <p>Frozen Food</p>
+      <div>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      <Parent>
+        <Image src={mouse} alt="mouse"/>
+        <Image src={mouse} alt="mouse"/>
+      </Parent>
+      </div>
     )
   }
 }
