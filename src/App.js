a57938c46884fe5e562e2319597226592e00ec13
Jonathan Clegg <jonathanclegg928@gmail.com>
2019-04-01 21:18:08 +0100

    make nav responsive at mobile size

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom'
+import {
+  Route,
+  HashRouter
+} from "react-router-dom";
 import Nav from './components/Nav'
 import Home from './pages/Home'
-import Crickets from './pages/Crickets'
-import Mealworms from './pages/Mealworms'
-import Rats from './pages/Rats'
-import Mice from './pages/Mice'
+import FrozenFood from './pages/FrozenFood'
+import Livefood from './pages/Livefood'
 import './App.css';
-
-const NavigationWithRouter = withRouter(Nav);
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <NavigationWithRouter/>
-          <Switch>
-            <Route path="/" component={Home} />
-            <Route path="/crickets" component={Crickets} />
-            <Route path="/mealworms" component={Mealworms} />
-            <Route path="/rats" component={Rats} />
-            <Route path="/mice" component={Mice} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <HashRouter>
+          <Nav/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/frozenfood" component={FrozenFood}/>
+            <Route path="/livefood" component={Livefood}/>
+      </HashRouter>
     );
   }
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,41 +1,82 @@
-import React from 'react'
-import styled from 'styled-components'
-import placeholder from '../assets/placeholder.png'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import placeholder from "../assets/placeholder.png";
+import { NavLink } from "react-router-dom";
 
 const StyledNav = styled.nav`
-    background: ${props => props.theme.colors.default};
-    display: flex;
-`
+  background: ${props => props.theme.colors.default};
+  display: block;
+`;
 
 const StyledTitle = styled.h2`
-    @media (max-width: 768px) {
-        font-size: 22px;
-    }
-`
+  @media (max-width: 768px) {
+    font-size: 22px;
+    display: inline-block;
+  }
+`;
 const Logo = styled.img`
-    width: 60px;
-    height: 60px;
-`
-const NavMenu = styled.div`
-  float: right;
-`
+  width: 60px;
+  height: 60px;
+`;
+const NavContainer = styled.div`
+
+`;
+
+const NavMenu = styled.ul`
+  @media (max-width: 768px) {
+  }
+`;
 const NavItem = styled(NavLink)`
   font-size: 20px;
   text-decoration: none;
   color: black;
   cursor: pointer;
-  margin-left: 20px;
-`
-const Nav = (props) => (
-    <StyledNav>
-        <Logo src={placeholder}/>
-        <StyledTitle>CompareTheReptile.com</StyledTitle>
-        <NavMenu>
-            <NavItem>Frozen Food</NavItem>
-            <NavItem>Live Food</NavItem>
-        </NavMenu>
-    </StyledNav>
-)
+  @media (max-width: 768px) {
+    display: block;
+    text-align: center;
+    margin-top: 15px;
+    padding-bottom: 10px;
+  }
+`;
 
-export default Nav
+const ToggleDiv = styled.div`
+  float: right;
+  margin-top: -70px;
+  margin-right: 25px;
+  box-sizing: border-box;
+  color: black;
+  font-size: 30px;
+  cursor: pointer;
+`;
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       showMenu: false
+    }
+  }
+  showMenu() {
+      this.setState({ showMenu: !this.state.showMenu });
+  }
+  render() {
+    let { showMenu } = this.state
+    return (
+      <StyledNav>
+        <Logo src={placeholder} />
+        <StyledTitle>CompareTheReptile.com</StyledTitle>
+        <NavContainer>
+          <ToggleDiv className="fa fa-bars menu" onClick={(event) => this.showMenu(event)}>
+          </ToggleDiv>
+            {showMenu &&
+            <NavMenu>
+              <NavItem to="/frozenfood">Frozen Food</NavItem>
+              <NavItem to="/livefood">Livefood</NavItem>
+            </NavMenu>
+            }
+        </NavContainer>
+      </StyledNav>
+    );
+  }
+}
+
+export default Nav;
