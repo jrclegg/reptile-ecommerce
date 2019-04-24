d4c3ca6dae0e477aa016c3e99c711a838f4f0037
Jonathan Clegg <jonathanclegg928@gmail.com>
2019-04-24 19:20:15 +0100

    display list for pinkie mice template

--- src/pages/Mice/Pinkies.js
+++ src/pages/Mice/Pinkies.js
@@ -22,96 +22,42 @@ class Pinkies extends React.Component {
         this.state = {
            products: []
         }
-      }
+    }
     
-      componentDidMount() {
+    componentDidMount() {
         data().then(data=>{ 
           this.setState({
             products: data.products
           })
         });
-      }
-    
+    }
+
     render() {
         this.items = this.state.products.map((item, key) =>
-        <div key={item.id}>
-        <MainTitle>{item.product_name}</MainTitle>
-        {console.log(item)}
-        <Parent>
-            <Image src={mouse}></Image>
-        </Parent>
-        <div>
-            <div>
-                <button>All</button>
-                <button>Single</button>
-                <button>Pack of 10</button>
-                <button>Pack of 25</button>
-                <button>Pack of 100</button>
+            <div key={item.id}>
+                <MainTitle>{item.product_name}</MainTitle>
+                <Parent>
+                    <Image src={mouse}></Image>
+                </Parent>
+                <div>
+                    <div>
+                        <button>All</button>
+                        <button>Single</button>
+                        <button>Pack of 10</button>
+                        <button>Pack of 25</button>
+                        <button>Pack of 100</button>
+                    </div>
+                    <SubTitle>{item.product_name} (Single)</SubTitle>
+                    {item.packs.map(pack =>
+                        <ul>
+                            {pack.retailers
+                                .sort((a,b) => a.price - b.price)
+                                .map(retailer => <li>{retailer.company_name}{retailer.price}</li>)
+                            }
+                        </ul>
+                    )}
+                </div>
             </div>
-            <SubTitle>{item.product_name} (Single)</SubTitle>
-            {item.retailers.map(retailer =>
-                <ul>
-                    {
-                        retailer.packs.map(pack => 
-                            pack.quantity === 1 ?
-                            <div>
-                                <h4 key={pack.id}>{retailer.company_name}</h4>
-                                {pack.price}
-                            </div>
-                            : ''
-                    )}
-                </ul>
-            )}
-        </div>
-        <div>
-            <SubTitle>{item.product_name} (Pack of 10)</SubTitle>
-            {item.retailers.map(retailer =>
-                <ul>
-                    {
-                        retailer.packs.map(pack => 
-                            pack.quantity === 10 ?
-                            <div>
-                                <h4 key={pack.id}>{retailer.company_name}</h4>
-                                {pack.price}
-                            </div>
-                            : ''
-                    )}
-                </ul>
-            )}
-        </div>
-        <div>
-            <SubTitle>{item.product_name} (Pack of 25)</SubTitle>
-            {item.retailers.map(retailer =>
-                <ul>
-                    {
-                        retailer.packs.map(pack => 
-                            pack.quantity === 25 ?
-                            <div>
-                                <h4 key={pack.id}>{retailer.company_name}</h4>
-                                {pack.price}
-                            </div>
-                            : ''
-                    )}
-                </ul>
-            )}
-        </div>
-        <div>
-            <SubTitle>{item.product_name} (Pack of 100)</SubTitle>
-            {item.retailers.map(retailer =>
-                <ul>
-                    {
-                        retailer.packs.map(pack => 
-                            pack.quantity === 100 ?
-                            <div>
-                                <h4 key={pack.id}>{retailer.company_name}</h4>
-                                {pack.price}
-                            </div>
-                            : ''
-                    )}
-                </ul>
-            )}
-        </div>
-        </div>
         );
         return (
             <div>
