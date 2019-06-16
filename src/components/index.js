e348abe91611fd5e05b1d823702b290c71ae8841
Jonathan Clegg <jonathanclegg928@gmail.com>
2019-06-16 13:23:09 +0100

    component index file

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import BasketButton from './BasketButton'
+import Image from './Image'
+import MainTitle from './MainTitle'
+import Nav from './Nav'
+import Parent from './Parent'
+import Product from './Product'
+import QuantityDescription from './QuantityDescription'
+import QuantityInput from './QuantityInput'
+import QuantityTitle from './QuantityTitle'
+import getProducts from './Product'
+
+export {
+    getProducts,
+    BasketButton,
+    Image,
+    MainTitle,
+    Nav,
+    Parent,
+    Product,
+    QuantityDescription,
+    QuantityInput,
+    QuantityTitle
+}
