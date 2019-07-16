8ad8ba02bcd79560914e999f163fe87c4cfa120b
Jonathan Clegg <jonathanclegg928@gmail.com>
2019-07-16 20:51:44 +0100

    pass render prop to route component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {data} from './../components/GetData';
+import {Image, 
+  QuantityDescription, QuantityTitle, 
+  QuantityInput, BasketButton, 
+  Parent, MainTitle} from '../components/index'
+  import mouse from '../assets/mouse.jpg'
 
   class GetProducts extends React.Component {
     state = {
-      products: []
+      products: [],
+      name: ''
     }
     componentDidMount() {
         data().then(data=>{ 
@@ -13,9 +19,35 @@ import {data} from './../components/GetData';
         });
     }
     render() {
-      return this.props.render(
-        this.state
-      );
+      return(
+      this.state.products.map((item) =>
+        <div key={item.id} >
+          {this.props.name === item.product_name ?
+          <React.Fragment>
+            <MainTitle>{item.product_name}</MainTitle>
+            <Parent>
+              <Image src={mouse}></Image>
+            </Parent>
+            {item.packs.map(pack =>
+              <div key={pack.quantity}>
+                  <QuantityTitle>Pack of {pack.quantity}</QuantityTitle>
+                  {pack.retailers
+                      .sort((a,b) => a.price - b.price)
+                      .map(retailer =>
+                          <div key={retailer.company_id}>
+                              <QuantityDescription>{retailer.company_name}<br/><br/>{retailer.price.toFixed(2)}<QuantityInput/><BasketButton>Add To Basket</BasketButton></QuantityDescription>
+                          </div>
+                      )
+                  }
+              </div>
+            )}
+            </React.Fragment>
+            : ''
+          }
+        </div>
+      )
+
+      )
     }
   }
 
