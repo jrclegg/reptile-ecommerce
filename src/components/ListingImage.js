b1daadb384fbb2b459e92489929571fedcae61f1
Jonathan Clegg <jonathanclegg928@gmail.com>
2020-05-16 13:25:33 +0100

    add mailchimp subscribe integration

---
+++ src/components/ListingImage.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const ListingImage = styled.img`
+    border: 1px solid #ccc;
+    box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+    width: 92%;
+    margin-top: 10px;
+`
+
+export default ListingImage
---
+++ src/components/Subscribe.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+    float: right;
+    margin-right: 50%;
+    margin-top: -150px;
+    @media (max-width: 768px) {
+      margin-right: 10%;
+    }
+`
+
+
+class Subscribe extends React.Component {
+  render() {
+    return (
+  <div id="mc_embed_signup">
+    <link
+    href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css"
+    rel="stylesheet"
+    type="text/css"
+  />
+    <Form
+      action="https://rocketmail.us18.list-manage.com/subscribe/post?u=bf927573eef714b0badfaad1e&id=079666ad20"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      className="validate"
+      target="_blank"
+      noValidate
+    >
+      <div id="mc_embed_signup_scroll">
+        <label style={{color: 'white', fontSize: '16px'}} htmlFor="mce-EMAIL">Subscribe</label>
+        <input
+          style={{width: '170px'}}
+          type="email"
+          name="EMAIL"
+          className="email"
+          id="mce-EMAIL"
+          placeholder="email address"
+          required
+        />
+        <div className="clear">
+          <input
+            style={{width: '170px'}}
+            type="submit"
+            defaultValue="Subscribe"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            className="button"
+          />
+        </div>
+      </div>
+    </Form>
+  </div>
+    );
+  }
+}
+
+export default Subscribe;
