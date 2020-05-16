c4695098b9e10e1c02ca58c3abce1fec23fc5ccb
Jonathan Clegg <jonathanclegg928@gmail.com>
2020-05-16 13:26:12 +0100

    add media queries to nav and images

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,8 +3,18 @@ import styled from 'styled-components'
 const Image = styled.img`
     border: 1px solid #ccc;
     box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
-    width: 80%;
-    margin-left: 8%;
+    width: 92%;
+    margin-top: 10px;
+    @media only screen and (min-width: 400px){
+        height: 25vh;
+    }
+    @media only screen and (min-width: 600px){
+        height: 40vh;
+    }
+    @media only screen and (min-width: 1024px){
+        height: 60vh;
+        width: 95%;
+    }
 `
 
 export default Image
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import placeholder from "../assets/placeholder.png";
 import { NavLink } from "react-router-dom";
-// import {scroller} from 'react-scroll';
+import HeaderSubscribe from './HeaderSubscribe'
 
 const Header = styled.div`
   background: ${props => props.theme.colors.default};
@@ -24,21 +24,43 @@ const Logo = styled.img`
 const NavList = styled.ul`
   @media (min-width: 768px) {
     padding-bottom: 20px;
+    position: relative;
+    display: inline-block;
+  }
+`
+const SubMenu = styled.ul`
+padding-top: 8px;
+  @media (min-width: 768px) {
+    display: none;
+    border: 1px solid ${props => props.theme.colors.default};;
+    background: #FFFFFF;
+    position: absolute;
+    top: 50px;
   }
 `
 const NavItem = styled(NavLink)`
   font-size: 20px;
   text-decoration: none;
+  white-space: nowrap;
   color: ${props => props.theme.colors.secondary};
-  font-weight: 800;
+  font-weight: bold;
   cursor: pointer;
   margin-left: 45px;
-  @media (max-width: 768px) {
+  display: block;
+  text-align: left;
+  margin-top: 15px;
+  padding-bottom: 10px;
+  margin-left: 30px;
+  @media only screen and (min-width: 768px) {
     display: block;
-    text-align: left;
-    margin-top: 15px;
-    padding-bottom: 10px;
-    margin-left: 30px;
+    top: 100px;
+    cursor: pointer;
+    &:hover ${SubMenu} {
+      display: block;
+    }
+    @media only screen and (min-width: 768px) {
+      display: block;
+    }
   }
 `
 const BasketLink = styled(NavLink)`
@@ -59,8 +81,31 @@ const BasketIcon = styled.i`
 
 `
 const NavItemDropdown = styled(NavItem)`
+  font-weight: bold;
+  display: block;
+  color: black;
+  margin-left: -20px;
+  margin-right: 20px;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+    color: white;
+  }
+  @media (min-width: 769px) {
+    margin-top: 0px;
+    &:hover {
+      text-decoration: underline;
+      color: ${props => props.theme.colors.default};
+    }
+  }
+
   @media (max-width: 768px) {
-   font-weight: normal;
+    position: relative;
+    margin-top: 15px;
+    color: white;
+    margin-left: -40px;
+    text-align: left;
+    text-decoration: none;
   }
 `
 const NavToggle = styled.div`
@@ -80,7 +125,6 @@ const NavToggle = styled.div`
 const NavItemArrow = styled.i`
   float: right;
   margin-right: 30px;
-  margin-top: -30px;
   color: white;
    @media (min-width: 768px) {
     display: none;
@@ -127,48 +171,55 @@ class Nav extends React.Component {
         <NavLink to="/">
           <MainTitle>CompareTheReptile.com</MainTitle>
         </NavLink>
-        <NavToggle id="navToggle" tabIndex="0" onClick={(event) => this.showMenu(event)}>
-          { showMenu === true ?
+        <NavToggle id="navToggle" onClick={(event) => this.showMenu(event)}>
+          { showMenu !== true ?
               <NavIcon className="fa fa-bars menu"></NavIcon>
               : <NavIcon className="fas fa-times"></NavIcon>
           }
         </NavToggle>
         <nav>
           { showMenu &&
+          <React.Fragment>
             <NavList>
-              <NavItem id="frozenFood" tabIndex="1" to="/frozenfood" onClick={(event) => this.toggleDropdown(event)}>
+              <NavItem id="frozenFood" to="/frozenfood" onClick={(event) => this.toggleDropdown(event)}>
                 Frozen Food
-              </NavItem>
               {
                     showFrozenItems === true ?
-                    <React.Fragment>
+                    <SubMenu>
                       <NavItemArrow className="fas fa-chevron-up"/>
-                      <NavDropdown url="/mice" productTitle="Mice" />
-                      <NavDropdown url="/rats" productTitle="Rats"/>
-                      <NavDropdown url="/multimammates" productTitle="Multimammates"/>
-                      <NavDropdown url="/guineapigs" productTitle="Guinea Pigs"/>
-                      <NavDropdown url="/rabbits" productTitle="Rabbits"/>
-                      <NavDropdown url="/hamsters" productTitle="Hamsters"/>
-                      <NavDropdown url="/gerbils" productTitle="Gerbils"/>
-                      <NavDropdown url="/chicks" productTitle="Chicks"/>
-                      <NavDropdown url="/quail" productTitle="Quail"/>
-                    </React.Fragment>
+                        <NavDropdown url="/mice" productTitle="Mice" />
+                        <NavDropdown url="/rats" productTitle="Rats"/>
+                        <NavDropdown url="/multimammates" productTitle="Multimammates"/>
+                        <NavDropdown url="/guineapigs" productTitle="Guinea Pigs"/>
+                        <NavDropdown url="/rabbits" productTitle="Rabbits"/>
+                        <NavDropdown url="/hamsters" productTitle="Hamsters"/>
+                        <NavDropdown url="/gerbils" productTitle="Gerbils"/>
+                        <NavDropdown url="/chicks" productTitle="Chicks"/>
+                        <NavDropdown url="/quail" productTitle="Quail"/>
+                    </SubMenu>
                     : <NavItemArrow className="fas fa-chevron-down"/>
                   }
-              <NavItem id="livefood" tabIndex="2" to="/livefood" onClick={(event) => this.toggleDropdown(event)}>
-                Livefood
               </NavItem>
+              </NavList>
+              <NavList>
+              <NavItem id="livefood" to="/livefood" onClick={(event) => this.toggleDropdown(event)}>
+                Livefood
               {
                 showLiveItems === true ? 
-                <React.Fragment>
+                <SubMenu>
                   <NavItemArrow className="fas fa-chevron-up"/>
                   <NavDropdown url="/locusts" productTitle="Locusts"/>
                   <NavDropdown url="/browncrickets" productTitle="Brown Crickets"/>
                   <NavDropdown url="/blackcrickets" productTitle="Black Crickets"/>
-                </React.Fragment>
+                  <NavDropdown url="/mealworms" productTitle="Mealworms"/>
+                  <NavDropdown url="/waxworms" productTitle="Waxworms"/>
+                </SubMenu>
                 : <NavItemArrow className="fas fa-chevron-down"/>
               }
+               </NavItem>
             </NavList>
+              <HeaderSubscribe/>
+            </React.Fragment>
           }
         </nav>
       </Header>
